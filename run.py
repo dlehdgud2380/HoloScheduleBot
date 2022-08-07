4e570afc55a539d1f414992b474c5eedb94ea792
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-08-08 00:00:39 +0900

    add: vtuber setup

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from sys import argv
 import os
+from module import twitter
+
+# holoEN vtuber id json file Setup
+twitter.setup_holoen_vtuber_id()
 
 # port mapping for uvicron
 PORT: int = int(argv[1]) if len(argv) > 1 else None
@@ -7,7 +11,7 @@ PORT: int = int(argv[1]) if len(argv) > 1 else None
 # launch command for uvicorn
 BASE_COMMAND = "uvicorn view:app --reload"
 
-# launch fastapit server
+# launch fastapi server
 if bool(PORT) is True:
     os.system(f"{BASE_COMMAND} --port {PORT}")
 else:
